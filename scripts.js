f8758cc0ff99a19fc879ea845295cb2320b16053
TiagoGrilloLermy <tlermy@gmail.com>
2023-07-14 16:56:38 -0300

    update javascript

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function abrir() {
+    document.querySelector("form").style.opacity = 1
+    document.querySelector("div").style.opacity = 0.3
+}
+function fechar() {
+    document.querySelector("form").style.opacity = 0
+    document.querySelector("div").style.opacity = 1
+}
